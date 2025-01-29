7f3ec72749530269db59674b5e440c7174d4400e
estebancastano <estebin5372@gmail.com>
2025-01-29 17:25:19 -0500

    Fix button styles

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -78,7 +78,7 @@ const Login: React.FC = () => {
                     </div>
                     <button
                         type="submit"
-                        className="w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        className="w-full px-4 py-2 text-sm font-medium text-gray-600 bg-indigo-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
                         Iniciar Sesión
                     </button>
