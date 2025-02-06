b263c36961d30d0ab2433c8bed2d56fcad1b14c5
estebancastano <estebin5372@gmail.com>
2025-02-06 15:33:59 -0500

    Add shoppinglist form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/SignUp";
+import ShoppingList from "./components/ShoppingList";
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/login" element={<Login />}/>
       <Route path="/register" element={<Register />} />
+      <Route path="/list" element={<ShoppingList />} />
     </Routes>
   </Router>
 );
